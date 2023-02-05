89fe1f4c9c19e4b1aa5eada8602e5412c34ec3cb
Diana <kat.lahuro.3223@gmail.com>
2023-02-05 11:46:31 +0300

    3

---
+++ 3/third.cs
@@ -0,0 +1,18 @@
+﻿System.Console.WriteLine("Введите цифру, обозначающую день недели");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+if (N < 1 || N > 7)
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    if (N == 6 || N == 7)
+    {
+        System.Console.WriteLine("Это выходной");
+    }
+    else
+    {
+        System.Console.WriteLine("Это  не выходной");
+    }
+}
