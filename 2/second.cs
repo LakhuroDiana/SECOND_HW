593e50c693a4292a08e5bdb79b98d6d2bc0d07ca
Diana <kat.lahuro.3223@gmail.com>
2023-02-05 11:46:12 +0300

    2

---
+++ 2/second.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int N = Convert.ToInt32(number);
+if (N / 100 == 0)
+{
+    System.Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(N % 1000 / 100);
+}
